1bbb3ed55d3f551c37d26cefd7daae50d5f405f6
Crowdbotics <engineering@crowdbotics.com>
2023-08-08 03:03:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frosty-grass-dev-107985",
+  url: "https://frosty-grass-dev-107985.botics.co",
+  api: "https://frosty-grass-dev-107985.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
